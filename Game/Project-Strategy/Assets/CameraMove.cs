42dc19bb1ecf769c311d1f0590a73680f34e2e3c
EPrograming <ethds24@gmail.com>
2017-07-13 16:44:52 -0400

    Add files via upload

---
+++ Game/Project-Strategy/Assets/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public float panSpeed = 10f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+	// Update is called once per frame
+	void Update () {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        
+
+        transform.position = pos;
+	}
+}
---
+++ Game/Project-Strategy/Assets/ZoomCamera.js
@@ -0,0 +1,12 @@
+﻿function Update () {
+    if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+    {
+        Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize-1, 1);
+
+
+    }
+    if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+    {
+        Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize-1, 6);
+    }
+}
