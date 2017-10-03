8d45a64a73bebca65937c28c9da225072dc4cb73
ProDevGuy <30932276+ProDevGuy@users.noreply.github.com>
2017-10-03 10:48:41 -0400

    Selectable.cs now changes the UI info
    Selectable.cs now changes ui info instaid of textchanger.cs
    Comment info
    textchanger.cs should now be deprecheated

--- Game/Project-Strategy/Assets/Scripts/Selectable.cs
+++ Game/Project-Strategy/Assets/Scripts/Selectable.cs
@@ -9,6 +9,10 @@ public class Selectable : MonoBehaviour {
     public string name;
     public GameObject panel;
     public int selected;
+    public TextMesh provinceNameText;
+	public TextMesh provinceOwnerText;
+	public Text provinceNameTextUI;
+	public Text provinceOwnerTextUI;
     void Start()
     {
 
@@ -45,11 +49,31 @@ public class Selectable : MonoBehaviour {
                 //check if mouse hit object with tag "construction
                 if (hitInfo.transform.gameObject.name == name)
                 {
+                    //tells game that a provinces is selected
                     selected = 1;
                     //Debug.Log("It's working!");
+                    
+                    //turns on the outline for the provinces script
                     booltest.eraseRenderer = false;
                     //panel.enabled = true;
+                    
+                    //turns on the UI panel
                     panel.SetActive(true);
+                    
+                    //checks the name of the provinces clicked, then changes panel info to reflect that.
+                    if(Name == "Ireland"){
+                        path = Application.streamingAssetsPath + "/Provinces/Ireland.json";
+                    }
+                    if(Name == "Wales"){
+                        path = Application.streamingAssetsPath + "/Provinces/Wales.json";
+                    }
+                    jsonString = File.ReadAllText(path);
+                    Province IRELAND = JsonUtility.FromJson<Province>(jsonString);
+                    provinceNameText = provinceNameText.GetComponent<TextMesh>();
+                    provinceNameTextUI = provinceNameTextUI.GetComponent<Text>();
+                    provinceNameText.text = IRELAND.name;
+                    provinceNameTextUI.text = IRELAND.name;
+                    
                     //GetComponent<cakeslice.Outline>().enabled = true;
 
                     //Debug.Log("This works two");
@@ -57,6 +81,7 @@ public class Selectable : MonoBehaviour {
                 else
                 {
                   //Debug.Log("nopz");
+                  //turns off the outline if player clicks provinces that is not this province
                   booltest.eraseRenderer = true;
                   selected = 0;
                   panel.SetActive(true);
@@ -65,6 +90,7 @@ public class Selectable : MonoBehaviour {
             else
             {
                 //Debug.Log("No hit");
+                //turns off outline if it does not hit something
                 booltest.eraseRenderer = true;
                 panel.SetActive(false);
                 selected = 0;
